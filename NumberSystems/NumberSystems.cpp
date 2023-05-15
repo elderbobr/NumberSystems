ebdb27146cdf1de8e3e73b3911405f788a7a8f8e
Makar <entoakk20@gmail.com>
2023-05-15 23:57:22 +0700

    Добавьте файлы проекта.

---
+++ NumberSystems/NumberSystems.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+void Translation(int& Number) {
+    int i;
+    while (Number != 0) {
+        i = 
+        Number --;
+    }
+}
+
+int main()
+{
+    int Number;
+    cin >> Number;
+    cout << Number << endl;
+
+}
