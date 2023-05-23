7b8f11f2f6860fdb863a040ba15e5174322279b2
ElderBobr <entoakk20@gmail.com>
2023-05-23 17:14:08 +0700

    Рабочая модель

--- NumberSystems/NumberSystems.cpp
+++ NumberSystems/NumberSystems.cpp
@@ -1,18 +1,36 @@
 ﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
 void Translation(int& Number) {
-    int i;
-    while (Number != 0) {
-        i = 
-        Number --;
+    int a = 2;
+    int i = 0;
+    while (Number > pow(a, i))
+    {
+        i++;
     }
+    int* arr = new int[i];
+    for (int j = 0; j < i; j++) {
+        if (Number % 2 == 0) {
+            arr[j] = 0;
+            Number /= 2;
+        }
+        else {
+            arr[j] = 1;
+            Number /= 2;
+        }
+    }
+
+    for (int j = i - 1; j >= 0; j--) {
+        cout << arr[j] << " ";
+    }
+
+    delete[] arr;
 }
 
 int main()
 {
     int Number;
     cin >> Number;
-    cout << Number << endl;
-
+    Translation(Number);
 }
